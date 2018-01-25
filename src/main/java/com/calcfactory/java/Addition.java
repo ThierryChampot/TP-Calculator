03fa917e8a2cd77a6d9349137d5f7ac33476e9da
ThierryChampot <thierry.champot@gmail.com>
2018-01-25 16:36:19 +0100

    code lint and doc

--- src/main/java/com/calcfactory/java/Addition.java
+++ src/main/java/com/calcfactory/java/Addition.java
@@ -1,27 +1,33 @@
-/**
- * 
- */
 package com.calcfactory.java;
 
 /**
+ * Addition logic and methods goes here.
  * @author Thierry
  *
  */
 public class Addition {
-	
-	protected int value = 0;
 
-	public Addition(int a) {
-		// TODO Auto-generated constructor stub
+	protected int value = 0;
+/**
+ * Takes the first number of the addition.
+ * @param a
+ */
+	public Addition(final int a) {
 		this.value = a;
 
 	}
-	
-	public void make(int b) {
+/**
+ * Takes the second number of the addition.
+ * @param b
+ */
+	public void make(final int b) {
 		this.value = this.value + b;
 	}
-	
-	public int getValue() {
+/**
+ *	Return the value of the result.
+ * @return
+ */
+	public final int getValue() {
 		return this.value;
 	}
 }
--- src/main/java/com/calcfactory/java/Application.java
+++ src/main/java/com/calcfactory/java/Application.java
@@ -1,26 +1,30 @@
 /**
- * 
+ *
  */
 package com.calcfactory.java;
 
 /**
+ * The main application entry and logic.
  * @author Thierry
  *
  */
-public class Application {
+public final class Application {
+
+	private Application () {}
 
 	/**
+	 * Short demo of what Calculator makes.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Addition add = new Addition(1);
-		
+
 		add.make(2);
 		System.out.println(add.getValue());
-		
+
 		add.make(3);
 		System.out.println(add.getValue());
-		
+
 		// TODO Auto-generated method stub
 		System.out.println("Hello World !!!");
 	}

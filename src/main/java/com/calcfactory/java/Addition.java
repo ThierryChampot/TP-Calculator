fc3cfef11acd755205d0a2dc8632c4f0c3a2b36b
ThierryChampot <thierry.champot@gmail.com>
2018-01-25 23:10:02 +0100

    lint code 2

--- src/main/java/com/calcfactory/java/Addition.java
+++ src/main/java/com/calcfactory/java/Addition.java
@@ -1,33 +1,36 @@
 package com.calcfactory.java;
 
 /**
- * Addition logic and methods goes here.
+ * Addition logic and methods goe here.
  * @author Thierry
  *
  */
 public class Addition {
 
-	protected int value = 0;
+    /**
+    * value is initialised at 0;
+    */
+    protected int value = 0;
 /**
  * Takes the first number of the addition.
- * @param a
+ * @param a The first number.
  */
-	public Addition(final int a) {
-		this.value = a;
+    public Addition(final int a) {
+        this.value = a;
 
-	}
+    }
 /**
  * Takes the second number of the addition.
- * @param b
+ * @param b The second number.
  */
-	public void make(final int b) {
-		this.value = this.value + b;
-	}
+    public void make(final int b) {
+        this.value = this.value + b;
+    }
 /**
  *	Return the value of the result.
- * @return
+ * @return The result of the addition.
  */
-	public final int getValue() {
-		return this.value;
-	}
+    public final int getValue() {
+        return this.value;
+    }
 }
--- src/main/java/com/calcfactory/java/Application.java
+++ src/main/java/com/calcfactory/java/Application.java
@@ -13,26 +13,26 @@ public final class Application {
 /**
  * The main application logic.
  */
-	private Application() { }
+    private Application() { }
 
-	/**
-	 * Short demo of what Calculator makes.
-	 * @param args
-	 */
-	public static void main(final String[] args) {
+    /**
+     * Short demo of what Calculator makes.
+     * @param args
+     */
+    public static void main(final String[] args) {
 
-		/**
-		 * add makes an Addition.
-		 */
-		final Addition add = new Addition(1);
+        /**
+         * add makes an Addition.
+         */
+        final Addition add = new Addition(1);
 
-		add.make(2);
-		System.out.println(add.getValue());
+        add.make(2);
+        System.out.println(add.getValue());
 
-		add.make(3);
-		System.out.println(add.getValue());
+        add.make(3);
+        System.out.println(add.getValue());
 
-		// TODO Auto-generated method stub
-		System.out.println("Hello World !!!");
-	}
+        // TODO Auto-generated method stub
+        System.out.println("Hello World !!!");
+    }
 }
